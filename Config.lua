37e8f7b1143da23e4922796912eea42ff14458b7
KarpoVelho <40266114+KarpoVelho@users.noreply.github.com>
2021-08-30 20:30:01 +0300

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,5 @@
+Config = {}
+Config.boliiseja = 0 --paljo kyttii alotuksee
+Config.blipaika = 5 --sekunteina kuin usein auton blippi päivittyy poliiseille
+Config.cooldown = 15 --minuutteina
+Config.tiirikointiaika = 25 --sekunteina kuinka kauan tiirikointi kestää.
